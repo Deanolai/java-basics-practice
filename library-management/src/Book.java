fc474c40ed31741f12d7f28710b157a25e296f3c
Deanolai <3208194348@qq.com>
2026-07-31 14:10:04 +0800

    feat: add Book entity

---
+++ library-management/src/Book.java
@@ -0,0 +1,57 @@
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private String category;
+    private boolean isBorrowed = false;
+
+    public Book() {
+
+    }
+    public Book(String isbn, String title, String author, String category) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+    @Override
+    public String toString() {
+        return "Book{isbn = '" + isbn + "', title = '" + title + "', author = '" + author + "', category = '" + category + "', isBorrowed = " + isBorrowed + "}";
+    }
+    public void borrow() {
+        isBorrowed = true;
+    }
+
+    public void returnBook() {
+        isBorrowed = false;
+    }
+}
+
