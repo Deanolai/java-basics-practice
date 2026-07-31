ce7b26781021f4a02507abd3a363f25c79a1181c
Deanolai <3208194348@qq.com>
2026-07-31 14:16:45 +0800

    refactor: remove unused no-arg constructor

--- library-management/src/Book.java
+++ library-management/src/Book.java
@@ -4,10 +4,7 @@ public class Book {
     private String author;
     private String category;
     private boolean isBorrowed = false;
-
-    public Book() {
-
-    }
+    
     public Book(String isbn, String title, String author, String category) {
         this.isbn = isbn;
         this.title = title;
